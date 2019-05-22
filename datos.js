68bcb2d97f9c7bde33bd5e505181b537f414ec6f
yenifer <yhyenifer@gmail.com>
2019-05-22 18:25:50 -0500

    Create datos.js

---
+++ datos.js
@@ -0,0 +1,25 @@
+let info = [];
+ info = [
+    {
+        id: 1,
+        nombre: 'español',
+        duracion: '1 mes',
+        valor: 100.000
+    },
+    {
+        id: 2,
+        nombre: 'quimica',
+        duracion: '1 mes',
+        valor: 100.000
+    },
+    {
+        id: 3,
+        nombre: 'sociales',
+        duracion: '2 mes',
+        valor: 500.000
+    }
+]
+
+module.exports = {
+    info
+}
